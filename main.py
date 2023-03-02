57ac27c8a767cbe0d1a33f910deebd313b9f779d
xqm32 <458173774@qq.com>
2023-03-02 13:51:09 +0800

    feat: unaudited users can also use

--- main.py
+++ main.py
@@ -3,66 +3,107 @@ import hashlib
 import json
 import pandas
 
-client = httpx.Client(follow_redirects=True)
 
-username, password = "", ""
-with open("config.json") as f:
-    config = json.load(f)
-    username, password = config["username"], config["password"]
+class EAMS:
+    base_url = "http://jxglstu.hfut.edu.cn/eams5-student"
 
-salt = client.get("http://jxglstu.hfut.edu.cn/eams5-student/login-salt").text
-password = hashlib.sha1((salt + "-" + password).encode()).hexdigest()
+    def __init__(self, config=None) -> None:
+        self.client = httpx.Client(follow_redirects=True)
 
-resp = client.post(
-    "http://jxglstu.hfut.edu.cn/eams5-student/login",
-    json={
-        "username": username,
-        "password": password,
-    },
-)
-assert resp.json()["result"] == True
+        if config is None:
+            username = input("username: ")
+            password = input("password: ")
+        else:
+            with open(config) as f:
+                config = json.load(f)
+                username = config["username"]
+                password = config["password"]
 
-resp = client.get(
-    "http://jxglstu.hfut.edu.cn/eams5-student/for-std/program-completion-preview"
-)
-json_url = str(resp.url).replace("info", "json")
-id = str(resp.url).rsplit("/", maxsplit=1)[1]
+        salt = self.client.get(f"{EAMS.base_url}/login-salt").text
+        password = hashlib.sha1((salt + "-" + password).encode()).hexdigest()
 
-# with open(f"{id}.json", "w") as f:
-#     resp = client.get(json_url)
-#     f.write(resp.text)
+        resp = self.client.post(
+            f"{EAMS.base_url}/login",
+            json={
+                "username": username,
+                "password": password,
+            },
+        )
 
-data_list = []
+        if resp.json()["result"] == False:
+            raise Exception("Login failed")
 
-resp = client.get(json_url)
-if resp.text == "null":
-# if True:
-    resp = client.get(
-        f"http://jxglstu.hfut.edu.cn/eams5-student/for-std/program/root-module-json/{id}"
-    )
-    data = resp.json()
+        resp = self.client.get(f"{EAMS.base_url}/for-std/program-completion-preview")
+        self.student_id = str(resp.url).rsplit("/", maxsplit=1)[1]
 
-    # with open(f"{id}.json", "w") as f:
-    #     f.write(resp.text)
+    def to_csv(self) -> None:
+        data_list = []
 
-    for i in data["children"]:
-        for j in i["planCourses"]:
-            data_list.append(
-                {
-                    "id": j["id"],
-                    "terms": j["terms"],
-                    "nameZh": j["course"]["nameZh"],
-                    "nameEn": j["course"]["nameEn"],
-                    "code": j["course"]["code"],
-                    "credits": j["course"]["credits"],
-                    "defaultExamMode": j["course"]["defaultExamMode"]["nameZh"],
-                    "type": i["type"]["nameZh"],
-                }
-            )
-else:
-    data = resp.json()
-    for i in data["moduleList"]:
-        data_list.extend(i["courseList"])
+        resp = self.client.get(
+            f"{EAMS.base_url}/for-std/program-completion-preview/json/{self.student_id}"
+        )
+        with open("preview.json", "w") as f:
+            f.write(resp.text)
 
-df = pandas.DataFrame(data_list)
-df.to_csv(f"{id}.csv", index=False)
+        if resp.text == "null":
+            data_list = self.__unadited()
+        else:
+            data = resp.json()
+            for i in data["moduleList"]:
+                data_list.extend(i["courseList"])
+
+        df = pandas.DataFrame(data_list)
+        df.to_csv(f"{self.student_id}.csv", index=False)
+
+    def __unadited(self) -> list:
+        data_list = []
+        grade = self.__grade()
+
+        resp = self.client.get(
+            f"http://jxglstu.hfut.edu.cn/eams5-student/for-std/program/root-module-json/{self.student_id}"
+        )
+        with open("module.json", "w") as f:
+            f.write(resp.text)
+
+        data = resp.json()
+        for i in data["children"]:
+            for j in i["planCourses"]:
+                code = j["course"]["code"]
+
+                if code not in grade.index:
+                    result_type = "UNREPAIRED"
+                    gp, remark = "", ""
+                else:
+                    result_type = "PASSED"
+                    gp = grade.loc[code]["绩点"]
+                    remark = grade.loc[code]["成绩"]
+
+                data_list.append(
+                    {
+                        "planCourseId": j["id"],
+                        "courseId": j["course"]["id"],
+                        "code": code,
+                        "nameZh": j["course"]["nameZh"],
+                        "nameEn": j["course"]["nameEn"],
+                        "credits": j["course"]["credits"],
+                        "terms": j["terms"],
+                        "compulsory": j["compulsory"],  # TODO 是否必修
+                        "resultType": result_type,
+                        "gp": gp,
+                        "rank": "",
+                        "remark": remark,
+                        "finalResultType": "",
+                    }
+                )
+
+        return data_list
+
+    def __grade(self) -> pandas.DataFrame:
+        resp = self.client.get(
+            f"{EAMS.base_url}/for-std/grade/sheet/info/{self.student_id}"
+        )
+        sheet = pandas.read_html(resp.text)
+        return pandas.concat(sheet).set_index("课程代码")
+
+
+EAMS("config.json").to_csv()
