0c2ac2fb8c9a1206f0814f43f4b55d114122c0c9
xqm32 <458173774@qq.com>
2023-03-02 11:43:18 +0800

    feat: training program for unaudited users

--- main.py
+++ main.py
@@ -5,13 +5,13 @@ import pandas
 
 client = httpx.Client(follow_redirects=True)
 
-username, password = '', ''
+username, password = "", ""
 with open("config.json") as f:
     config = json.load(f)
     username, password = config["username"], config["password"]
 
 salt = client.get("http://jxglstu.hfut.edu.cn/eams5-student/login-salt").text
-password = hashlib.sha1((salt + '-' + password).encode()).hexdigest()
+password = hashlib.sha1((salt + "-" + password).encode()).hexdigest()
 
 resp = client.post(
     "http://jxglstu.hfut.edu.cn/eams5-student/login",
@@ -20,11 +20,11 @@ resp = client.post(
         "password": password,
     },
 )
-assert(resp.json()["result"] == True)
+assert resp.json()["result"] == True
 
-resp = client.get("http://jxglstu.hfut.edu.cn/eams5-student/for-std/program-completion-preview")
-assert(resp.text != "null")
-
+resp = client.get(
+    "http://jxglstu.hfut.edu.cn/eams5-student/for-std/program-completion-preview"
+)
 json_url = str(resp.url).replace("info", "json")
 id = str(resp.url).rsplit("/", maxsplit=1)[1]
 
@@ -32,12 +32,37 @@ id = str(resp.url).rsplit("/", maxsplit=1)[1]
 #     resp = client.get(json_url)
 #     f.write(resp.text)
 
-resp = client.get(json_url)
-data = resp.json()
-
 data_list = []
-for i in range(6):
-    data_list.extend(data["moduleList"][i]["courseList"])
+
+resp = client.get(json_url)
+if resp.text == "null":
+# if True:
+    resp = client.get(
+        f"http://jxglstu.hfut.edu.cn/eams5-student/for-std/program/root-module-json/{id}"
+    )
+    data = resp.json()
+
+    # with open(f"{id}.json", "w") as f:
+    #     f.write(resp.text)
+
+    for i in data["children"]:
+        for j in i["planCourses"]:
+            data_list.append(
+                {
+                    "id": j["id"],
+                    "terms": j["terms"],
+                    "nameZh": j["course"]["nameZh"],
+                    "nameEn": j["course"]["nameEn"],
+                    "code": j["course"]["code"],
+                    "credits": j["course"]["credits"],
+                    "defaultExamMode": j["course"]["defaultExamMode"]["nameZh"],
+                    "type": i["type"]["nameZh"],
+                }
+            )
+else:
+    data = resp.json()
+    for i in data["moduleList"]:
+        data_list.extend(i["courseList"])
 
 df = pandas.DataFrame(data_list)
 df.to_csv(f"{id}.csv", index=False)
